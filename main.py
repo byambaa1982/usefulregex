0b4c7c3741cb2abfd0fadc1806ae1d8ef56c2f03
Byambatsogt Enkhbat <enkhbat@Byambatsogts-MacBook-Pro.local>
2020-02-07 14:08:32 -0600

    change main.py

--- main.py
+++ main.py
@@ -1,53 +1,8 @@
-import regex as re
-#--------string list--------------------
-test=['23','byamba','df23','@12-435-(323)','nan','1000M', 23,'--', np.nan]
+import pandas as pd 
+import numpy as np 
 
 
-#------ Pick only numbers---------
-def only_number(my_list):
-  result=[]
-  for i in range(len(test)):
-    try:
-    	#--- use regex here--------------
-      result.append(re.sub('[^0-9,]', "", test[i]))
-    except:
-      result.append(test[i])
-  return result
 
-#------ String to numberic---------
-  def to_numberic(my_list):
-  my_list=only_number(my_list)
-  to_numbers=[]
-  for i in range(len(my_list)):
-    try:
-      # print(my_list[i])
-      to_numbers.append(float(my_list[i]))
-    except:
-      to_numbers.append(np.nan)
-  return to_numbers
-
-#---------Remove comma and turn string into intenger-------
-#--------------"700,000" into 700000-------------
-
-def comma_str_to_int(a_str):
-  try:
-    a_str=re.sub(",", "", a_str)
-    a_num=re.sub('[^0-9]', "", a_str)
-    a_num=int(a_num)
-  except:
-    a_num=np.nan
-  return a_num
-#-----------------If there is a point in string this function
-#-------------------turn the string into a float
-#----------------------For example: "$23.4" into 23.4
-
-def point_str_to_float(a_str):
-  try:
-    a_num=re.findall(r"[-+]?\d*\.\d+|\d+", a_str)
-    a_num=float(a_num[0])
-  except:
-    a_num=np.nan
-  return a_num
 
 #--------------no regex version---------------
 # ---------------turn any string into foat-------------
@@ -55,17 +10,20 @@ def anystring_to_float(string):
   newstring ="" 
   my_float=""
   count=0
-  for a in string: 
-      if a=='.' or (a.isnumeric()) == True: 
-          count+= 1
-          my_float+=a
-      else: 
-          newstring+= a 
-  print(count) 
-  print(newstring) 
-  print('data type of {} is now {}'.format(num, type(num))
-)
-  return float(my_float)
+  try:
+    for a in string: 
+        if a=='.' or (a.isnumeric()) == True: 
+            count+= 1
+            my_float+=a
+        else: 
+            newstring+= a 
+    # print(count) 
+    # print(newstring) 
+    # print('data type of {} is now {}'.format(num, type(num)))
+    return float(my_float)
+  except:
+    return np.nan
+
 
 # anystring_to_float(string)
 
@@ -75,3 +33,10 @@ def change_df(df):
     print(df.columns[i])
     df[df.columns[i]]=df[df.columns[i]].map(lambda row:anystring_to_float(row))
   return df
+
+
+#--------You should change indice list here: ---------
+
+
+indice_of_columns=[5,7,8,9]
+change_df(df)
